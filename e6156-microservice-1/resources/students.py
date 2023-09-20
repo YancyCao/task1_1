78c135d855a4bda819db2ae40f315183d3cda735
YancyCao <128929018+YancyCao@users.noreply.github.com>
2023-09-20 16:46:47 -0400

    Update students.py

--- e6156-microservice-1/resources/students.py
+++ e6156-microservice-1/resources/students.py
@@ -8,7 +8,7 @@ class StudentsResource:
     #
 
     students_file = \
-        r".\resources\old-students.json"
+        ".\resources\old-students.json"
 
     def __init__(self):
         self.students = None
